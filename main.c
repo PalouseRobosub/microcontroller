3b0514a01288fe880838ba3e60d04d401f99195c
irwineffect <irwineffect@gmail.com>
2013-09-26 16:09:04 -0700

    added device configuration, commented out unnecessary code

--- main.c
+++ main.c
@@ -13,29 +13,33 @@
 #include "Testbench.h"
 #include "Queue.h"
 
+/*processor configuration*/
+#pragma config FPLLMUL = MUL_20, FPLLIDIV = DIV_2, FPLLODIV = DIV_1, FWDTEN = OFF
+#pragma config POSCMOD = HS, FNOSC = PRIPLL, FPBDIV = DIV_8
+
  I2C_STATE i2c_state;
  Queue I2CQ;
 
 int main(void)
 {
-    queueTest();
+    //queueTest();
 
     
-    i2c_state = SENDING_START;
+    //i2c_state = SENDING_START;
     //setup buttons
-    PORTSetPinsDigitalIn(IOPORT_A, BIT_6 | BIT_7);
+    //PORTSetPinsDigitalIn(IOPORT_A, BIT_6 | BIT_7);
     
     //setup LEDs
-    PORTSetPinsDigitalOut(IOPORT_B, BIT_10 | BIT_11);
+    //PORTSetPinsDigitalOut(IOPORT_B, BIT_10 | BIT_11);
 
-    setup_i2c(I2C1);
+    //setup_i2c(I2C1);
 
 
     while (1)
     {
 
 
-        i2c_1_isr();
+        //i2c_1_isr();
 //        current_state = read_buttons();
 //
 //        write_leds(current_state);
