f6eeec2d426d1e1b3ea79b439b972d61fc5fc286
ibrech <ian.brech@eecs.wsu.edu>
2013-09-27 18:33:50 -0700

    Created header and source file for UART ISR

---
+++ UART_ISR.c
@@ -0,0 +1,149 @@
+/********************************************************
+ *   File Name: functions.c
+ *
+ *   Description:
+ *              Main source code file
+ *
+ *
+ *********************************************************/
+#include "functions.h"
+#include "UART_ISR.h"
+
+/*************************************************************************
+ Variables
+ ************************************************************************/
+UART_QUEUE I2C_1_Queue;
+
+ /********************************************************
+ *   Function Name:
+ *
+ *   Description:
+ *
+ *
+ *********************************************************/
+void uart_setup(UART_MODULE uart_id)
+{
+    U1BRG = 259; //if main clock is 40 MHz, use  259. 10MHz, use 64
+    U1MODEbits.ON = 1;
+
+    //Setup UART1 interrupts
+}
+
+/********************************************************
+ *   Function Name:
+ *
+ *   Description:
+ *
+ *
+ *********************************************************/
+inline void uart_begin(void)
+{
+
+}
+
+/********************************************************
+ *   Function Name:
+ *
+ *   Description:
+ *
+ *
+ *********************************************************/
+void uart_InitializeQueue(UART_QUEUE* queue)
+{
+
+}
+
+/********************************************************
+ *   Function Name:
+ *
+ *   Description:
+ *
+ *
+ *********************************************************/
+int uart_addToQueue(UART_QUEUE* queue, UART_NODE new_node)
+{
+
+}
+
+/********************************************************
+ *   Function Name:
+ *
+ *   Description:
+ *
+ *
+ *********************************************************/
+int uart_popNode(UART_QUEUE* queue, UART_NODE* return_node)
+{
+
+}
+
+ /********************************************************
+ *   Function Name:
+ *
+ *   Description:
+ *
+ *
+ *********************************************************/
+void send_data(UART_MODULE uart_id, UINT8* bsndData, UINT16 size)
+{
+    int i;
+
+    for(i = 0; i < size; i++)
+    {
+        while(!UARTTransmitterIsReady(uart_id));
+
+        UARTSendDataByte(uart_id, bsndData[i]);
+
+        while(!UARTTransmissionHasCompleted(uart_id));
+    }
+}
+
+ /********************************************************
+ *   Function Name:
+ *
+ *   Description:
+ *
+ *
+ *********************************************************/
+void read_data(UART_MODULE uart_id)
+{
+    int i = 0;
+
+    while(UARTReceivedDataIsAvailable(uart_id))
+    {
+        brecvData[i] = UARTGetDataByte(uart_id);
+        i++;
+    }
+
+}
+
+ /********************************************************
+ *   Function Name:
+ *
+ *   Description:
+ *
+ *
+ *********************************************************/
+void __ISR(_UART1_VECTOR, ipl2) IntUart1Handler(void)
+{
+	// Is this an RX interrupt?
+	if(INTGetFlag(INT_SOURCE_UART_RX(UART_ID)))
+	{
+            // Clear the RX interrupt Flag
+	    INTClearFlag(INT_SOURCE_UART_RX(UART_ID));
+
+            //get data
+            read_data(UART_ID);
+
+            // Echo what we just received.
+            send_data(UART_ID, brecvData, sizeof(brecvData));
+	}
+
+	// TX interrupt
+	if ( INTGetFlag(INT_SOURCE_UART_TX(UART_ID)) )
+	{
+            // Clear the TX interrupt Flag
+            INTClearFlag(INT_SOURCE_UART_TX(UART_ID));
+
+	}
+}
---
+++ UART_ISR.h
@@ -0,0 +1,77 @@
+/*
+ * File:   uart.h
+ * Author: Ian
+ *
+ * Created on September 27, 2013, 10:34 AM
+ */
+
+#ifndef UART_H
+#define	UART_H
+
+/*************************************************************************
+ System Includes
+ ************************************************************************/
+#include <peripheral/ports.h>
+#include <peripheral/uart.h>
+
+/*************************************************************************
+ Typedefs
+ ************************************************************************/
+
+/*************************************************************************
+ Constants
+ ************************************************************************/
+#define UARTQueueSize 16
+#define BAUD_RATE 9600 //UxBRG = 119
+#define UART_ID UART1
+/*************************************************************************
+ Structure Definitions
+ ************************************************************************/
+typedef struct uart_node
+{
+    uint8 uart_data[4];
+//    uint8 sensor_id;
+//    uint8 sensor_sub_id;
+//    uint8 data_h;
+//    uint8 data_l;
+}UART_NODE;
+
+typedef struct uart_queue
+{
+    UART_NODE DataBank[UARTQueueSize];
+    uint8 QueueStart;
+    uint8 QueueEnd;
+    uint8 QueueLength;
+}UART_QUEUE;
+
+/*************************************************************************
+ Enums
+ ************************************************************************/
+
+/*************************************************************************
+ Variables
+ ************************************************************************/
+UINT8* brecvData;
+UINT16 brevSize;
+
+/*************************************************************************
+ Function Declarations
+ ************************************************************************/
+
+ /********************************************************
+ *   Function Name:
+ *
+ *   Description:
+ *
+ *
+ *********************************************************/
+void uart_setup(UART_MODULE uart_id);
+inline void uart_begin(void);
+void uart_InitializeQueue(UART_QUEUE* queue);
+int uart_addToQueue(UART_QUEUE* queue, UART_NODE new_node);
+int uart_popNode(UART_QUEUE* queue, UART_NODE* return_node);
+void send_data(UART_MODULE uart_id, UINT8* bsndData, UINT16 size);
+void read_data(UART_MODULE uart_id);
+
+#endif	/* UART_H */
+
