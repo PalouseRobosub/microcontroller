b288d9ff3ca8408603c8249757db98be12b0cf75
korey <tkdmaster92@hotmail.com>
2013-10-04 20:58:32 -0700

    working on uart Rx

--- UART_ISR.c
+++ UART_ISR.c
@@ -14,28 +14,35 @@
  ************************************************************************/
 UART_QUEUE UART_1_Queue;
 boolean UART1_is_idle;
- /********************************************************
+
+/********************************************************
  *   Function Name:
  *
  *   Description:
  *
  *
  *********************************************************/
-void uart_setup(void)
-{
+void uart_setup(void) {
     U1BRG = 64; //if main clock is 40 MHz, use  259. 10MHz, use 64
     U1MODEbits.PDSEL = 0;
     uart_InitializeQueue(&UART_1_Queue);
 
 
     //Setup UART1 TX interrupts
-    IEC0SET = (1 << 28); //enable interrupt
+    IEC0SET = (1 << 28); //enable interrupt Tx
     IPC6SET = (2 << 2); //set priority
-    U1STAbits.UTXEN = 1;
-    U1MODEbits.ON = 1;
-//    uart_InitializeQueue(&UART_1_Queue);//initialize the queue
+    U1STAbits.UTXISEL = 2; //Set interrupt to fire when transmit buffer is empty
+    U1STAbits.UTXEN = 1; //enable Tx
+    
+    //Setup UART1 RX interrupts
+    IEC0SET = (1 << 27); //enable interrupt Rx
+    //set by default - U1STAbits.URXISEL = 0; //Set interrupt to fire when a character is recieved
+    U1STAbits.URXEN = 1; //enable Rx
 
-    UART1_is_idle = TRUE;//set that bus is currently idle
+    U1MODEbits.ON = 1;
+    //    uart_InitializeQueue(&UART_1_Queue);//initialize the queue
+
+    UART1_is_idle = TRUE; //set that bus is currently idle
 }
 
 /********************************************************
@@ -45,8 +52,7 @@ void uart_setup(void)
  *
  *
  *********************************************************/
-inline void uart_begin(void)
-{
+inline void uart_begin(void) {
     /* this sets the UART1TX interrupt flag. Setting the flag will
      cause the ISR to be entered as soon as the global interrupt
      flag is enabled */
@@ -60,9 +66,8 @@ inline void uart_begin(void)
  *
  *
  *********************************************************/
-void uart_InitializeQueue(UART_QUEUE* queue)
-{
-    memset(queue, 0, sizeof(UART_QUEUE));
+void uart_InitializeQueue(UART_QUEUE* queue) {
+    memset(queue, 0, sizeof (UART_QUEUE));
 }
 
 /********************************************************
@@ -72,20 +77,15 @@ void uart_InitializeQueue(UART_QUEUE* queue)
  *
  *
  *********************************************************/
-int uart_addToQueue(UART_QUEUE* queue, UART_NODE new_node)
-{
-    if (queue->QueueEnd == queue->QueueStart && queue->QueueLength > 0)
-    {
+int uart_addToQueue(UART_QUEUE* queue, UART_NODE new_node) {
+    if (queue->QueueEnd == queue->QueueStart && queue->QueueLength > 0) {
         return 1; //Error, would overwrite start of list
     }
     queue->DataBank[queue->QueueEnd] = new_node;
     queue->QueueLength++;
-    if (queue->QueueEnd == UARTQueueSize-1)
-    {
+    if (queue->QueueEnd == UARTQueueSize - 1) {
         queue->QueueEnd = 0;
-    }
-    else
-    {
+    } else {
         queue->QueueEnd++;
     }
     return 0;
@@ -98,65 +98,77 @@ int uart_addToQueue(UART_QUEUE* queue, UART_NODE new_node)
  *
  *
  *********************************************************/
-int uart_popNode(UART_QUEUE* queue, UART_NODE* return_node)
-{
-    if (queue->QueueLength == 0)
-    {
+int uart_popNode(UART_QUEUE* queue, UART_NODE* return_node) {
+    if (queue->QueueLength == 0) {
         return 1; //Can't read from queue if empty
     }
     *return_node = (queue->DataBank[queue->QueueStart]); //Returns the Node
-    if (queue->QueueStart == UARTQueueSize-1)
-    {
+    if (queue->QueueStart == UARTQueueSize - 1) {
         queue->QueueStart = 0;
-    }
-    else
-    {
+    } else {
         queue->QueueStart++;
     }
     queue->QueueLength--;
     return 0;
 }
 
- /********************************************************
+/********************************************************
  *   Function Name: _UART_1_Handler
  *
  *   Description:
  *             ISR for the UART1 module
  *
  *********************************************************/
-void __ISR(_UART1_VECTOR, ipl2) IntUart1Handler(void)
-{
+void __ISR(_UART1_VECTOR, ipl2) IntUart1Handler(void) {
     int i, k;
-    UART_NODE current_node;
+    static UART_NODE current_node;
+    static uint8 data_index;
 
-    if(uart_popNode(&UART_1_Queue, &current_node))
-    {
-        UART1_is_idle = TRUE;
+    //URXDA is 1 if recieve buffer has data
+    //TRMT is 1 if transmit buffer is empty
+
+    if (IFS0bits.U1RXIF == 1) {
+
+        //U1RXREG is the recieve register that data will come into
+        IFS0bits.U1RXIF = 0; //clear the interrupt flag
     }
-    else
-    {
-      for(i = 0; i < 4; i++)
-        {
-//            U1STAbits.UTXBRK = 1;
-            U1TXREG = current_node.uart_data[i];
-            for(k = 0; k < 500; k++); //delay to keep the FIFO from overflowing
-//            U1STAbits.UTXBRK = 0;
-        }
-        UART1_is_idle = FALSE;
-    }
+    if (IFS0bits.U1TXIF == 1) {
+
+        ++data_index;
+
+         if (data_index == 4) //if we have sent all data bytes
+         {
+             data_index = 0;
+         }
+         else //if we have more bytes to send
+         {
+             U1TXREG = current_node.uart_data[data_index];
+         }
+        /*if (uart_popNode(&UART_1_Queue, &current_node)) {
+            UART1_is_idle = TRUE;
+        } else {
+            for (i = 0; i < 4; i++) {
+                //            U1STAbits.UTXBRK = 1;
+                U1TXREG = current_node.uart_data[i];
+                for (k = 0; k < 500; k++); //delay to keep the FIFO from overflowing
+                //            U1STAbits.UTXBRK = 0;
+            }
+            UART1_is_idle = FALSE;
+        }*/
 
         IFS0bits.U1TXIF = 0; //clear the interrupt flag
+    }
+
 }
 
- /********************************************************
+/********************************************************
  *   Function Name: uart_SetNode( uint Byte1, uint Byte2, uint Byte3 )
  *
  *   Description: Creates a node using three bytes
  *
  *
  *********************************************************/
- void uart_CreateNode( uint Byte1, uint Byte2, uint Byte3 )
- {
+void uart_CreateNode(uint Byte1, uint Byte2, uint Byte3) {
     UART_NODE temp;
 
     temp.uart_data[0] = Byte1;
@@ -165,4 +177,4 @@ void __ISR(_UART1_VECTOR, ipl2) IntUart1Handler(void)
     temp.uart_data[3] = '\n';
 
     uart_addToQueue(&UART_1_Queue, temp);
- }
+}
