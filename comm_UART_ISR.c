3969fbfbbea8c6eb004ba190b9fe750698e8542b
korey <tkdmaster92@hotmail.com>
2013-10-31 21:09:52 -0700

    motor commands

--- comm_UART_ISR.c
+++ comm_UART_ISR.c
@@ -20,6 +20,7 @@ boolean SYNC_LOCK;
 uint8 received_bytes[6];
 uint8 received_index;
 boolean begin_sync;
+boolean packet_recieved;
 
 /********************************************************
  *   Function Name:
@@ -37,6 +38,8 @@ void comm_uart_setup(void) {
     //initialize variables
     memset(received_bytes, 0, sizeof (received_bytes));
     received_index = 0;
+    packet_recieved = FALSE;
+    SYNC_LOCK = FALSE;
 
 
     //Setup UART1 TX interrupts
@@ -48,16 +51,12 @@ void comm_uart_setup(void) {
 
     //Setup UART1 RX interrupts
     COMM_UART_RX_INT_set(1); //enable interrupt Rx
-    
+
     //set by default - U1STAbits.URXISEL = 0; //Set interrupt to fire when a character is recieved
     COMM_UART_URXEN = 1; //enable Rx
 
     COMM_UART_ON = 1;
     //    uart_InitializeQueue(&COMM_UART_Queue);//initialize the queue
-
-    SYNC_LOCK = FALSE;
-    begin_sync = TRUE;
-
 
     COMM_UART_is_idle = TRUE; //set that bus is currently idle
 }
@@ -136,8 +135,7 @@ int comm_uart_popNode(COMM_UART_QUEUE* queue, COMM_UART_NODE* return_node) {
  *             ISR for the COMM_UART module
  *
  *********************************************************/
-void __ISR(_COMM_UART_VECTOR, IPL7AUTO) comm_uart_Handler(void)
-{
+void __ISR(_COMM_UART_VECTOR, IPL7AUTO) comm_uart_Handler(void) {
     int i, k;
     COMM_UART_NODE current_node;
     INTDisableInterrupts();
@@ -148,39 +146,37 @@ void __ISR(_COMM_UART_VECTOR, IPL7AUTO) comm_uart_Handler(void)
     //led_val = ~led_val;
     //URXDA is 1 if recieve buffer has data
     //TRMT is 1 if transmit buffer is empty
-    if (COMM_UART_RXIF == 1)
-
-    {
+    if (COMM_UART_RXIF == 1) {
         received_byte = COMM_UART_RXREG;
-        
+        write_leds(received_byte - '0'); //write to LEDs to test UART Rx
+        if (received_byte == 'P') {
+            comm_uart_CreateNode('P', 0, 0);
+            comm_uart_CreateNode('Q', COMM_UART_Queue.QueueLength, 0);
+            // write_leds(0xFF);
+        }
+
         if (SYNC_LOCK) //if in sync
         {
             if (received_index == 0)//only check the first byte for the control byte
             {
-                if (received_byte != CONTROL_BYTE)
-                {
+                if (received_byte != CONTROL_BYTE) {
                     SYNC_LOCK = FALSE;
                     //CALL SYNC FUNCTION
                 }
                 received_index++;
-            }
-            else if(received_index == 1) //NOTE only indexes 1 and 2 are used
+            } else if (received_index == 1) //NOTE only indexes 1 and 2 are used
             {
                 received_bytes[received_index] = received_byte;
                 received_index++;
-            }
-            else if(received_index == 2)
-            {
+            } else if (received_index == 2) {
                 received_bytes[received_index] = received_byte;
                 received_index = 0; //Reset the index, we now have the full package
+                packet_recieved = TRUE;
             }
-        }
-        else //we need to aquire sync
-        {
-            if (begin_sync == TRUE )
-            {
+        } else {
+            if (begin_sync == TRUE) {
                 received_index = 0;
-                begin_sync = FALSE;
+                begin_sync = TRUE;
             }
 
             received_bytes[received_index] = received_byte;
@@ -188,22 +184,143 @@ void __ISR(_COMM_UART_VECTOR, IPL7AUTO) comm_uart_Handler(void)
 
             //X00X00
             //012345
-            if (received_index == 6)
-            {
-                if (    received_bytes[0] == CONTROL_BYTE &&
+            if (received_index == 6) {
+                if (received_bytes[0] == CONTROL_BYTE &&
                         received_bytes[3] == CONTROL_BYTE
-                   )
-                {
+                        ) {
                     SYNC_LOCK = TRUE;
+                    received_index = 0;
+                } else {
+                    begin_sync = TRUE;
                 }
-
-                begin_sync = TRUE;
-                received_index = 0;
             }
         }
 
         //MOVE THIS TO THE MAIN PROCESSING FUNCTION
+        if (packet_recieved) {
+            con_led(0, FALSE);
+            con_led(1, FALSE);
+            con_led(2, FALSE);
+            con_led(3, FALSE);
+            con_led(4, FALSE);
+            con_led(5, FALSE);
+            con_led(6, FALSE);
+            con_led(7, FALSE);
+            con_led(8, FALSE);
+            con_led(9, FALSE);
+            con_led(10, FALSE);
+            con_led(11, FALSE);
+            switch (received_bytes[1]) {
+                case THRUSTER_BOW_SB:
+                    if (received_bytes[2] & 0x80) //Pull off the direction bit
+                    {
+                        Motor1_Forward(129, (received_bytes[2] & 0x7F));
+                        con_led(0, FALSE);
+                        con_led(1, FALSE);
+                        con_led(2, TRUE);
+                        con_led(3, FALSE);
+                    } else {
+                        Motor1_Backward(129, (received_bytes[2] & 0x7F));
+                        con_led(0, FALSE);
+                        con_led(1, FALSE);
+                        con_led(2, TRUE);
+                        con_led(3, TRUE);
+                    }
+                    break;
+                case THRUSTER_BOW_PORT:
+                    if (received_bytes[2] & 0x80) //Pull off the direction bit
+                    {
+                        Motor1_Forward(128, (received_bytes[2] & 0x7F));
+                        con_led(0, FALSE);
+                        con_led(1, TRUE);
+                        con_led(2, FALSE);
+                        con_led(3, FALSE);
+                    } else {
+                        Motor1_Backward(128, (received_bytes[2] & 0x7F));
+                        con_led(0, FALSE);
+                        con_led(1, TRUE);
+                        con_led(2, FALSE);
+                        con_led(3, TRUE);
+                    }
+                    break;
+                case THRUSTER_STERN_SB:
+                    if (received_bytes[2] & 0x80) //Pull off the direction bit
+                    {
+                        Motor2_Forward(129, (received_bytes[2] & 0x7F));
+                        con_led(0, FALSE);
+                        con_led(1, TRUE);
+                        con_led(2, TRUE);
+                        con_led(3, FALSE);
+                    } else {
+                        Motor2_Backward(129, (received_bytes[2] & 0x7F));
+                        con_led(0, FALSE);
+                        con_led(1, TRUE);
+                        con_led(2, TRUE);
+                        con_led(3, TRUE);
+                    }
+                    break;
+                case THRUSTER_STERN_PORT:
+                    if (received_bytes[2] & 0x80) //Pull off the direction bit
+                    {
+                        Motor2_Forward(128, (received_bytes[2] & 0x7F));
+                        con_led(0, TRUE);
+                        con_led(1, FALSE);
+                        con_led(2, FALSE);
+                        con_led(3, FALSE);
+                    } else {
+                        Motor2_Backward(128, (received_bytes[2] & 0x7F));
+                        con_led(0, TRUE);
+                        con_led(1, FALSE);
+                        con_led(2, FALSE);
+                        con_led(3, TRUE);
+                    }
+                    break;
+                case THRUSTER_DEPTH_SB:
+                    if (received_bytes[2] & 0x80) //Pull off the direction bit
+                    {
+                        Motor2_Forward(130, (received_bytes[2] & 0x7F));
+                        con_led(0, TRUE);
+                        con_led(1, FALSE);
+                        con_led(2, TRUE);
+                        con_led(3, FALSE);
+                    } else {
+                        Motor2_Backward(130, (received_bytes[2] & 0x7F));
+                        con_led(0, TRUE);
+                        con_led(1, FALSE);
+                        con_led(2, TRUE);
+                        con_led(3, TRUE);
+                    }
+                    break;
+                case THRUSTER_DEPTH_PORT:
+                    if (received_bytes[2] & 0x80) //Pull off the direction bit
+                    {
+                        Motor1_Forward(130, (received_bytes[2] & 0x7F));
+                        con_led(0, TRUE);
+                        con_led(1, TRUE);
+                        con_led(2, FALSE);
+                        con_led(3, FALSE);
+                    } else {
+                        Motor1_Backward(130, (received_bytes[2] & 0x7F));
+                        con_led(0, TRUE);
+                        con_led(1, TRUE);
+                        con_led(2, FALSE);
+                        con_led(3, TRUE);
+                    }
+                    break;
 
+            }
+            /*
+             * Motor1_Forward( uint address, uint speed )
+             *
+            #define THRUSTER_BOW_SB 	 0x10
+            #define THRUSTER_BOW_PORT 	 0x11
+            #define THRUSTER_DEPTH_SB 	 0x12
+            #define THRUSTER_DEPTH_PORT  0x13
+            #define THRUSTER_STERN_SB 	 0x14
+            #define THRUSTER_STERN_PORT  0x15
+             */
+
+        }
 
         //COMM_UART_RXIF is the recieve register that data will come into
         COMM_UART_RXIF = 0; //clear the interrupt flag
@@ -239,7 +356,7 @@ void comm_uart_CreateNode(uint Byte1, uint Byte2, uint Byte3) {
     temp.uart_data[1] = Byte1;
     temp.uart_data[2] = Byte2;
     temp.uart_data[3] = Byte3;
-    
+
 
     comm_uart_addToQueue(&COMM_UART_Queue, temp);
 }
