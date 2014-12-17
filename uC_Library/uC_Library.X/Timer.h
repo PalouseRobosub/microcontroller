1d4057e2a6970960f1c891ffc66a04ab8a92ade3
ryan-summers <summers.ryan.m@gmail.com>
2014-12-17 12:44:53 -0800

    Addition of Timer Files
    This commit adds in the files for timer functionality in the microcontroller library

---
+++ uC_Library/uC_Library.X/Timer.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   Timer.h
+ * Author: Ryan Summers
+ *
+ * Created on December 17, 2014, 12:36 PM
+ */
+
+#ifndef TIMER_H
+#define	TIMER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+/* #Define Functions / Inline Functions*/
+    //begin
+
+/* Function Prototypes*/
+    //setup
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TIMER_H */
+
