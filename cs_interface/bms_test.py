64ced8e9446ceeb87c47d72bdc06d9091b5bc3ce
ryan-summers <summers.ryan.m@gmail.com>
2015-04-25 13:16:29 -0700

    This commit adds in BMS test code

--- cs_interface/bms_test.py
+++ cs_interface/bms_test.py
@@ -22,3 +22,34 @@ def main():
 if __name__ == "__main__":
         main()
 
+#from python.packetizer import Packetizer
+#import os
+#import signal
+#import serial
+#
+#
+#def convert(char1, char2):
+#                bits = 16
+#                char1 = char1 << 8
+#                char1 = char1 | char2
+#                if (char1 & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
+#                        char1 = char1 - (1 << bits)                # compute negative value
+#                return char1
+#
+#	# Main program
+#def main():
+#	packet = []
+#	p = Packetizer('/dev/ttyUSB3')	
+#	p.get_lock()
+#	while (1):
+#		os.system('clear')
+#		packet = p.receive_pakcet()
+#		value = convert(packet[1], packet[2])
+#		current = value*3.3/1024*26.2/6.2/1027/.00025*0.8998
+#		print "ID: " + packet[0] + " Value: " + current
+#
+#
+## Junk required to call main first
+#if __name__ == "__main__":
+#                main()
+
