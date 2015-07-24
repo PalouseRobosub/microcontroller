bfbde00b55868554f388b0d879df64462eae768d
Connor-Henning <connor.henning@email.wsu.edu>
2015-07-24 13:03:06 -0700

    made minor changes

--- LED/pattern.c
+++ LED/pattern.c
@@ -4,6 +4,10 @@ extern uint8 led_buf[NUMLEDS*3 + (NUMLEDS-1)/32 + 1] = {0};
 extern PIXEL led_frame_buf[12][NUMLEDS] = {0};
 extern PIXEL *pixels = (PIXEL *) led_buf;
 
+uint8 init;
+uint8 led_frames[10];
+uint8 number_frames;
+
 
 //timer subroutine
 void Pattern_Select(uint8 *msg)
@@ -18,12 +22,15 @@ void Pattern_Select(uint8 *msg)
     pattern_timer.period      = *period * 58.6;
     pattern_timer.enabled     = 1;
 
+    init = 1;
+
     switch (pattern)
     {
         case reset_period:
             PR2 = *period * 58.6;
         case forward_warp_shift:
             forward_warp_shift ();
+            pattern_timer.callback() = forward_warp_shift;
             break;
         case backward_warp_shift:
             backward_warp_shift ();
@@ -40,11 +47,57 @@ void Pattern_Select(uint8 *msg)
         default:
             break;
     }
+    init = 0;
     if(pattern_timer.period == 0)
         initialize_timer(pattern_timer);
 }
+
+void flash()
+{
+    static int i = 0;
+
+    if(init)
+        i = 0;
+
+    for(i = 0; i < number_frames; ++i)
+    {
+        memcpy(led_buf, led_frame_buf[led_frames[i]]);
+    }
+    if(i >= number_frames)
+        i = 0;
+    send_SPI(SPI_CH_1, led_buf, sizeof(led_buf));
+}
+
+void blink()
+{
+    static int i = 0;
+
+    if(init)
+        i = 0;
+
+    for(i = 0; i < (number_frames << 2); ++i)
+    {
+        if(i | 1)
+            memcpy(led_buf, led_frame_buf[0]);
+        else
+            memcpy(led_buf, led_frame_buf[led_frames[i>>2]]);
+    }
+    if(i >= number_frames)
+        i = 0;
+    send_SPI(SPI_CH_1, led_buf, sizeof(led_buf));
+}
+
 void forward_warp_shift ()
 {
+    if(init)
+    {
+        memcpy(led_buf, led_frame_buf[led_frames[0]][i]);
+        send_SPI(SPI_CH_1, led_buf, sizeof(led_buf));
+    }
+    else
+    {
+
+    }
     PIXEL tempL, tempR;
     uint8 i = 0;
     tempL = pixels [7];
@@ -59,7 +112,7 @@ void forward_warp_shift ()
     }
     pixels [0] = tempL;
     pixels [15] = tempR;
-    send_SPI(SPI_CH_1, led_buf, sizeof(led_buf));
+    
 }
 
 void backward_warp_shift ()
