e937c016c871604a0a5861525fda879fed7c2e83
ryan-summers <summers.ryan.m@gmail.com>
2015-08-15 16:15:21 -0700

    External Interrupts Complete And Library Header File Added
    This commit has the finalized external interrupts placed in it.
    This also contains the addition of the file sublibinal.h,
    which simply contains includes on every other header file in the library.
    This is to simplify including the library in other projects.

--- uC_Library/ExtInterrupts.h
+++ uC_Library/ExtInterrupts.h
@@ -27,34 +27,21 @@ extern "C" {
         Pin_RPB3,
         Pin_RPB4,
         Pin_RPB15,
-        Pin_RPB7,
-        Pin_RPC7,
-        Pin_RPC0,
-        Pin_RPC5, // End Int4
+        Pin_RPB7,// End Int4
         Pin_RPA1, //Start Int3
         Pin_RPB5,
         Pin_RPB1,
         Pin_RPB11,
-        Pin_RPB8,
-        Pin_RPA8,
-        Pin_RPC8,
-        Pin_RPA9, //End Int3
+        Pin_RPB8,//End Int3
         Pin_RPA2, //Start Int2
-        Pin_RPB6,
         Pin_RPA4,
         Pin_RPB13,
-        Pin_RPB2,
-        Pin_RPC6,
-        Pin_RPC1,
-        Pin_RPC3, // End Int2
+        Pin_RPB2,// End Int2
         Pin_RPA3, //Start Int1
         Pin_RPB14,
         Pin_RPB0,
         Pin_RPB10,
-        Pin_RPB9,
-        Pin_RPC9,
-        Pin_RPC2,
-        Pin_RPC4, // End Int1
+        Pin_RPB9,// End Int1
     }Pin;
     
     typedef enum {
@@ -62,19 +49,25 @@ extern "C" {
         rising
     } Polarity;
     
+    typedef enum {
+        none,
+        pullup,
+        pulldown
+    } Resistor;
+    
     typedef struct INTERRUPT_CONFIG{
         Interrupt extInt;
         Pin pin;
         Polarity polarity;
-        int priority;
-        int subPriority;
         int enable;
+        Resistor resistor;
         void *callback;
     } Interrupt_Config;
     
     void initialize_INT(Interrupt_Config config);
     void disable_INT(Interrupt extInt);
     void enable_INT(Interrupt extInt);
+    
 
 #ifdef	__cplusplus
 }
---
+++ uC_Library/sublibinal.h
@@ -0,0 +1,31 @@
+/* 
+ * File:   sublibinal.h
+ * Author: ryan
+ *
+ * Created on August 15, 2015, 3:21 PM
+ */
+
+#ifndef SUBLIBINAL_H
+#define	SUBLIBINAL_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include "ADC.h"
+#include "ExtInterrupts.h"
+#include "I2C.h"
+#include "Queue.h"
+#include "SPI.h"
+#include "Sensor.h"
+#include "System.h"
+#include "Timer.h"
+#include "UART.h"
+#include "packetizer.h"
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SUBLIBINAL_H */
+
